d04c132ad2dca1d10c0771f3dc700b044978ab21
Gabriel Coelho <gabriel.coelho.hgc@outlook.com.br>
2025-10-16 15:57:45 -0300

    Sync

---
+++ micro-company/src/main/java/com/ocoelhogabriel/microcompany/MicroCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.ocoelhogabriel.microcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroCompanyApplication.class, args);
+	}
+
+}
